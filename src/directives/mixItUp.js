38fc0781154f5696f9a13c012b7396f810c363bf
Sobin George Thomas <sobingt@gmail.com>
2015-07-23 21:26:20 +0530

    added login, mentor and nav styles

--- src/directives/mixItUp.js
+++ src/directives/mixItUp.js
@@ -9,9 +9,10 @@ angular.module('Codegurukul')
 
     function link (scope, element, attrs) {
       $(element).mixItUp();
-      var color=["#ed5565", "#fc6e51", "#ffce54",  "#48cfad",  "#4fc1e9",  "#5d9cec", "#ec87c0",  "#aab2bd",  "#f5f7fa"];
       $('span.btn.filter').each(function(i){
-            $(this).css('border-color',color[i % color.length]);
+            var j = i % 7;
+            j = j+1;
+            $(this).addClass('color'+j);
       });
     }
   });
